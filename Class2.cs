be9758f1b479679e70b6511f896a316f9ec3c51a
Prashant Singh <PrashantSingh@gmail.com>
2026-09-06 04:32:07 +0530

    Added class2.cs

---
+++ Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalConsoleApplication
+{
+    internal class Class2
+    {
+        public void Fun3()
+        {
+            Console.WriteLine("First Change");
+            Console.WriteLine("Second Chaange");
+
+            Console.WriteLine("third change");
+
+            Console.WriteLine("Fourth change");
+
+            Console.WriteLine("Fifth Chnage");
+
+        }
+    }
+}
